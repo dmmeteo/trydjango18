e476dc2f4b5170f3f6c47bb9c4ed476d0fe26053
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2016-08-19 15:27:52 +0300

    Changed views for work with couchdb, create new document with values and etc.

--- couch/views.py
+++ couch/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from . import forms
+from .forms import CouchForm
 import django_couch
+from django.shortcuts import redirect
+from django.utils import timezone
 
 
 # Create your views here.
 def home(request):
 
     # Added our form.
-    form = forms.CouchForm
+    form = CouchForm(request.POST or None)
 
     # Define a couchdb source
     db = django_couch.db('db')
@@ -17,7 +19,19 @@ def home(request):
     # result = db.create(data)
 
     # Fetching a data from couchdb.
-    query = db.view('sub/subs').rows
+    query = db.view('sub/sub').rows
+
+    # Checking a form
+    if form.is_valid():
+        title = form.cleaned_data.get('title')
+        link = form.cleaned_data.get('link')
+
+        # Sending our data to the couchdb
+        data = {"title": title, "link": link, "type": "subscriptions"}
+
+        # Create our document in couchdb
+        db.create(data)
+        return redirect('couch:home')
 
     # Define our context, that we'll send.
     context = {
