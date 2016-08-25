2c9668d1349c33cd92b286d24df7645372e8aa1d
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2016-08-26 00:58:54 +0300

    Changed urls for filter app

--- filters/urls.py
+++ filters/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
-from django.core.urlresolvers import reverse_lazy
-from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('filters:add'))),
+    url(r'^$', views.home, name='home'),
     url(r'^add/$', views.add, name='add'),
     url(r'^conf/$', views.conf, name='conf')
 ]
