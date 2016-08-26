19270f0a81ee603348dc95ab674599aa312c6877
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2016-08-26 17:19:54 +0300

    Changed urls

--- filters/urls.py
+++ filters/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^add/$', views.add, name='add'),
-    url(r'^conf/$', views.conf, name='conf')
+    url(r'^conf/$', views.conf, name='conf'),
+    url(r'^parsed/(?P<doc_id>\w+)/$', views.filter_parser, name='parsed')
 ]
