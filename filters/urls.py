a1574546e86a643d02c3ce8a5f2866ce23854296
Vladyslav <vladyslav.krylasov@gmail.com>
2016-08-31 12:57:35 +0300

    Added a new url path

--- filters/urls.py
+++ filters/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^add/$', views.add, name='add'),
     url(r'^conf/$', views.conf, name='conf'),
-    url(r'^parsed/(?P<doc_id>\w+)/$', views.filter_parser, name='parsed')
+    url(r'^parsed/(?P<doc_id>\w+)/$', views.filter_parser, name='parsed'),
+    url(r'^update/(?P<doc_id>\w+)/$', views.update, name='update'),
 ]
