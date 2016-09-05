2f61f4d32dc85caec0540e1a6a8dac7ab9ba29bd
Vladyslav <vladyslav.krylasov@gmail.com>
2016-09-05 16:21:15 +0300

    Added magic init method that sends db and user name into form

--- aggregator/forms.py
+++ aggregator/forms.py
@@ -6,6 +6,7 @@ class AddRssSource(forms.Form):
     title = forms.CharField(max_length=255)
     link = forms.CharField(max_length=500)
 
+
 # Define our choices for filter's form
 items = (
     ('title', 'Title'),
@@ -20,6 +21,16 @@ actions = (
 
 # FiltersForm is print out title, two select elements and an one required textinput's field
 class FiltersForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        db = kwargs.pop('db', None)
+        user = kwargs.pop('user', None)
+        response = db.view('subscriptions/sources', key=str(user)).rows
+        super(FiltersForm, self).__init__(*args, **kwargs)
+        self.fields['sources'] = forms.MultipleChoiceField(widget=forms.SelectMultiple(attrs={'class': 'selectpicker'}),
+                                                           choices=[
+                                                               (item.id, item.value['title']) for item in response
+                                                           ])
+
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'A title'}), label='Title')
     item = forms.ChoiceField(widget=forms.Select(attrs={'class': 'selectpicker'}), required=False, label='If',
                              choices=items)
